0b6bb39d481ca15d923cdaca3b38284f9ad380a1
Johnny Wu <jonathan.x.wu@gmail.com>
2023-12-18 21:45:04 -0500

    Adding SST

---
+++ src/app/components/ClientComponent.tsx
@@ -0,0 +1,86 @@
+import {AreaChart, Card, Flex, Icon, Metric, NumberInput, ProgressBar, Text, Title} from "@tremor/react";
+import {ArrowCircleDownIcon, CurrencyDollarIcon, CurrencyEuroIcon} from "@heroicons/react/outline";
+import {ReactElement} from "react";
+
+const chartdata = [
+	{
+		date: "Jan 2023",
+		USD: 18000,
+		SATS: 100000,
+	},
+	{
+		date: "Feb 2023",
+		USD: 20000,
+		SATS: 100000,
+	},
+	{
+		date: "Mar 2023",
+		USD: 26000,
+		SATS: 100000,
+	},
+	{
+		date: "Apr 2023",
+		USD: 22500,
+		SATS: 100000,
+	},
+	{
+		date: "May 2023",
+		USD: 30000,
+		SATS: 100000,
+	},
+	{
+		date: "Jun 2023",
+		USD: 35000,
+		SATS: 100000,
+	},
+];
+
+const valueFormatter = function(number: number) {
+	return "$ " + new Intl.NumberFormat("us").format(number).toString();
+};
+
+interface ClientProps {
+	data: any;
+}
+export function ClientComponent(props: ClientProps): ReactElement {
+	const data = props.data;
+	console.log(data);
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-self-start p-24">
+			<Card className="max-w-xs my-4 mx-auto">
+				<Flex className="mt-4">
+					<Text>$USD Exchange Rate</Text>
+					<Metric className="text-base">$1.00</Metric>
+				</Flex>
+				<Flex className="mt-4 mx-auto">
+					<Icon className="w-8 h-8 text-center" icon={ArrowCircleDownIcon} />
+				</Flex>
+				<Flex className="mt-4">
+					<Text>$EUR Exchange Rate</Text>
+					<Metric className="text-base">$1.04</Metric>
+				</Flex>
+				<ProgressBar value={32} className="mt-2" />
+			</Card>
+			<Card className="max-w-xs my-4 mx-auto">
+				<Text>Swap</Text>
+				<NumberInput className="my-2" icon={CurrencyDollarIcon} placeholder="Send.." />
+				<NumberInput className="my-2"icon={CurrencyEuroIcon} placeholder="Receive..." />
+				<Flex className="mt-4">
+					<Text>1% fee</Text>
+					<Text>Delivery 1 hr</Text>
+				</Flex>
+			</Card>
+			<Card>
+				<Title>Price Over Time</Title>
+				<AreaChart
+					className="h-72 mt-2"
+					data={chartdata}
+					index="date"
+					categories={["USD", "SATS"]}
+					colors={["green", "red"]}
+					valueFormatter={valueFormatter}
+				/>
+			</Card>
+		</main>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,28 @@
-"use client";
-import { Card, Text, Metric, Flex, ProgressBar, NumberInput } from "@tremor/react";
-import { AreaChart, Title, Icon } from "@tremor/react";
-import { CurrencyDollarIcon, CurrencyEuroIcon, ArrowCircleDownIcon } from '@heroicons/react/outline';
+"use server";
 
+import crypto from 'crypto';
+import useSWR from 'swr';
 
-const chartdata = [
-  {
-    date: "Jan 2023",
-    USD: 18000,
-    SATS: 100000,
-  },
-  {
-    date: "Feb 2023",
-    USD: 20000,
-    SATS: 100000,
-  },
-  {
-    date: "Mar 2023",
-    USD: 26000,
-    SATS: 100000,
-  },
-  {
-    date: "Apr 2023",
-    USD: 22500,
-    SATS: 100000,
-  },
-  {
-    date: "May 2023",
-    USD: 30000,
-    SATS: 100000,
-  },
-  {
-    date: "Jun 2023",
-    USD: 35000,
-    SATS: 100000,
-  },
-];
+import { ClientComponent } from './components/ClientComponent';
 
-const valueFormatter = function(number: number) {
-  return "$ " + new Intl.NumberFormat("us").format(number).toString();
+const getItem = async () => {
+  const nonce = Date.now();
+  const request = {
+    book: 'btc_mxn', // btc_usd btc_usdt
+  }
+  const sharedSecret = nonce + 'get' + '/api/v3/orders' + JSON.stringify(request);
+  const query = "book=btc_mxn";
+  const signature = crypto.createHmac("sha256", sharedSecret).update(query).digest("hex");
+  const res = await fetch("https://api.bitso.com/api/v3/orders?" + query, {
+    headers: {
+      "X-Signature": signature
+
+    }
+  })
+  return res.json()
 };
 
-const Home = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-self-start p-24">
-      <Card className="max-w-xs my-4 mx-auto">
-        <Flex className="mt-4">
-          <Text>$USD Exchange Rate</Text>
-          <Metric className="text-base">$1.00</Metric>
-        </Flex>
-        <Flex className="mt-4 mx-auto">
-          <Icon className="w-8 h-8 text-center" icon={ArrowCircleDownIcon} />
-        </Flex>
-        <Flex className="mt-4">
-          <Text>$EUR Exchange Rate</Text>
-          <Metric className="text-base">$1.04</Metric>
-        </Flex>
-        <ProgressBar value={32} className="mt-2" />
-      </Card>
-      <Card className="max-w-xs my-4 mx-auto">
-        <Text>Swap</Text>
-        <NumberInput className="my-2" icon={CurrencyDollarIcon} placeholder="Send.." />
-        <NumberInput className="my-2"icon={CurrencyEuroIcon} placeholder="Receive..." />
-        <Flex className="mt-4">
-          <Text>1% fee</Text>
-          <Text>Delivery 1 hr</Text>
-        </Flex>
-      </Card>
-      <Card>
-        <Title>Price Over Time</Title>
-        <AreaChart
-          className="h-72 mt-2"
-          data={chartdata}
-          index="date"
-          categories={["USD", "SATS"]}
-          colors={["green", "red"]}
-          valueFormatter={valueFormatter}
-        />
-      </Card>
-    </main>
-  )
+export default async function Home() {
+  // const { data, error, isLoading } = useSWR('/api/v3/orders', getItem)
+  return <ClientComponent data={{}} />
 }
-
-export default Home
