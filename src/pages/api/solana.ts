7cfae8c90b40079084e5a22a0ba6dcc5cd8b2b6c
Johnny Wu <jonathan.x.wu@gmail.com>
2024-01-03 17:58:55 -0500

    Add chain specific file naming conventions

--- src/pages/api/solana.ts
+++ src/pages/api/solana.ts
@@ -1,6 +1,7 @@
 const solanaWeb3 = require("@solana/web3.js");
 const axios = require("axios");
 
+// TEST DEV NET
 const rpc = "https://solana-mainnet.g.alchemy.com/v2/DZG0-YVM1DypFQSBtmfw4ejBWE8p3bPf"; // RPC URL for connecting with a Solana node
 
 const main = async () => {
