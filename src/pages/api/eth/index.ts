d443fdf9d82873fc7db36b6dd5ef822f1e9a4953
Johnny Wu <jonathan.x.wu@gmail.com>
2024-01-05 14:20:58 -0500

    Split out send transaction code to its own file

---
+++ src/pages/api/eth/index.ts
@@ -0,0 +1,11 @@
+const { Alchemy, Network, Wallet } = require("alchemy-sdk");
+
+export const settings = {
+	apiKey: 'Gou-7qtS5tPJuLE90NkfVa7-T2sjWVhF', // Replace with your Alchemy API Key.
+	network: Network.ETH_SEPOLIA, // Replace with your network.
+};
+
+export const pk = process.env.ALCHEMY_PRIVATE_KEY || ''
+export const alchemy = new Alchemy(settings);
+
+export const wallet = new Wallet(pk);
