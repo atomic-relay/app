8f1e54c3c3977a81fa17bf90e405fee668320b65
Johnny Wu <jonathan.x.wu@gmail.com>
2024-01-11 16:18:30 -0500

    Update form and conversion logic

--- src/components/ClientComponent.tsx
+++ src/components/ClientComponent.tsx
@@ -1,11 +1,11 @@
 "use client"
 import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input';
-import { Button, Card, Flex, Icon, Metric, NumberInput, Text, TextInput } from "@tremor/react";
-import {ArrowCircleDownIcon, CurrencyDollarIcon, PhoneIcon, MailIcon } from "@heroicons/react/outline";
+import {Button, Card, Divider, Flex, Metric, NumberInput, Text, TextInput} from "@tremor/react";
+import { CurrencyDollarIcon, PhoneIcon, MailIcon } from "@heroicons/react/outline";
 import {ReactElement, useState, useEffect} from "react";
 import { Money } from 'ts-money'
 import { useRouter } from 'next/router'
+import { Select, SelectItem } from "@tremor/react";
 
 interface ClientProps {
 	data?: any;
@@ -14,39 +14,70 @@ export function ClientComponent(props: ClientProps): ReactElement {
 	const data = props.data;
 	const router = useRouter();
 	const [dollar, setDollars] = useState(0);
+	const [btc, setBTC] = useState(0);
 	const [euro, setEuros] = useState(0);
+	const [mxn, setPesos] = useState(0);
 	const [fee, setFee] = useState(0);
+	const [currency, setCurrency] = useState('usd');
 	const [phone, setPhone] = useState();
 
-	const exchangeRate = 1.1;
+	const btcDollarRate = 0.000022;
 	const fixedFee = .01;
 	useEffect(() => {
 		if (dollar) {
 			const dollarMoney = new Money(dollar * 100, 'USD')
-			setEuros(Math.round(dollarMoney.amount * exchangeRate / 100));
+			setEuros(Math.round(dollarMoney.amount * 1.1 / 100));
+			setPesos(Math.round(dollarMoney.amount * 16.91 / 100));
+			setBTC(dollarMoney.amount * btcDollarRate / 100);
 			setFee(dollarMoney.amount * fixedFee / 100);
 		}
 	}, [dollar]);
 
 	const query = `usd=${dollar}&eur=${euro}`;
 
+	const usFormat = new Intl.NumberFormat('en-US');
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-self-start p-24">
 			<Card className="max-w-sm my-4 mx-auto">
 				<NumberInput className="my-2" icon={CurrencyDollarIcon} onValueChange={(val => setDollars(val))} placeholder="Send.." enableStepper={false} />
-				<TextInput icon={MailIcon} className="my-2" error={false} placeholder="Email..." />
-				<NumberInput icon={PhoneIcon} placeholder="+18001234" enableStepper={false} />
+
+				<Divider />
 				<Flex className="mt-2">
 					<Text className="text-base">$USD</Text>
-					<Metric className="text-base">${dollar}</Metric>
-				</Flex>
-				<Flex className="mt-2 mx-auto">
-					<Icon className="w-8 h-8 text-center" icon={ArrowCircleDownIcon} />
+					<Metric className="text-base">${usFormat.format(dollar)}</Metric>
 				</Flex>
 				<Flex className="mt-2">
-					<Text className="text-base">$EUR</Text>
-					<Metric className="text-base">${euro}</Metric>
+					<Text className="text-base">€EUR</Text>
+					<Metric className="text-base">€{usFormat.format(euro)}</Metric>
 				</Flex>
+				<Flex className="mt-2">
+					<Text className="text-base">₱MXN</Text>
+					<Metric className="text-base">₱{usFormat.format(mxn)}</Metric>
+				</Flex>
+				<Flex className="mt-2">
+					<Text className="text-base">₿BTC</Text>
+					<Metric className="text-base">₿{usFormat.format(btc)}</Metric>
+				</Flex>
+				<Divider />
+				<div className="max-w-sm mx-auto space-y-6">
+					<Select value={currency} onValueChange={setCurrency}>
+						<SelectItem value="usd" icon={CurrencyDollarIcon}>
+							US Dollar
+						</SelectItem>
+						<SelectItem value="eur" icon={CurrencyDollarIcon}>
+							Euro Dollar
+						</SelectItem>
+						<SelectItem value="mxn" icon={CurrencyDollarIcon}>
+							Mexican Pesos
+						</SelectItem>
+						<SelectItem value="btc" icon={CurrencyDollarIcon}>
+							Bitcoin
+						</SelectItem>
+					</Select>
+				</div>
+				<TextInput icon={MailIcon} className="my-2" error={false} placeholder="Email..." />
+				<NumberInput icon={PhoneIcon} placeholder="+18001234" enableStepper={false} />
+				<Divider />
 				<Button className="max-w-lg my-2" onClick={() => router.push('/confirmation?' + query)}>Submit</Button>
 				<Flex className="mt-4">
 					<Text>1% fee estimate {fee > 0 && `$`}{fee > 0 && fee}</Text>
