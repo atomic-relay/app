297d7d7e486dcd5093e4bfa90e999014eca0ed7b
Johnny Wu <jonathan.x.wu@gmail.com>
2024-01-29 13:39:12 +0400

    Add back in custom document.tsx

---
+++ src/app/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/*<script src="https://cdn.metricalp.com/event/metricalp.js" data-tid="mam6" defer></script>*/}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <Script
+          src="https://cdn.metricalp.com/event/metricalp.js"
+          data-tid="mam6"
+          defer
+          onError={() => console.error("error")}
+          onReady={() => console.log("true")}
+        />
+      </body>
+    </Html>
+  );
+}
