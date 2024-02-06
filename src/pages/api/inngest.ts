4ad85750cc6cd33cac34eb105933cdabc0d176b2
Johnny Wu <jonathan.x.wu@gmail.com>
2024-02-06 13:22:59 -0500

    move tigerbeetle to long running inngest file api endpoint

--- src/pages/api/inngest.ts
+++ src/pages/api/inngest.ts
@@ -1,5 +1,11 @@
+//@ts-nocheck
 import { Inngest } from "inngest";
 import { serve } from "inngest/next";
+import {
+  createClient,
+  CreateAccountError,
+  CreateTransferError,
+} from "tigerbeetle-node";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
@@ -10,6 +16,88 @@ const helloWorld = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
     return { event, body: "Hello, World!" };
+  },
+);
+
+const tigerBeetle = inngest.createFunction(
+  { id: "tigerbeetle" },
+  { event: "tigerbeetle" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    const client = createClient({
+      cluster_id: 0n,
+      replica_addresses: [process.env.TB_ADDRESS || "3000"],
+    });
+    let accountErrors = await client.createAccounts([
+      {
+        id: 1n,
+        debits_pending: 0n,
+        debits_posted: 0n,
+        credits_pending: 0n,
+        credits_posted: 0n,
+        user_data_128: 0n,
+        user_data_64: 0n,
+        user_data_32: 0,
+        reserved: 0,
+        ledger: 1,
+        code: 1,
+        flags: 0,
+        timestamp: 0n,
+      },
+      {
+        id: 2n,
+        debits_pending: 0n,
+        debits_posted: 0n,
+        credits_pending: 0n,
+        credits_posted: 0n,
+        user_data_128: 0n,
+        user_data_64: 0n,
+        user_data_32: 0,
+        reserved: 0,
+        ledger: 1,
+        code: 1,
+        flags: 0,
+        timestamp: 0n,
+      },
+    ]);
+    for (const error of accountErrors) {
+      console.error(
+        `Batch account at ${error.index} failed to create: ${CreateAccountError[error.result]}.`,
+      );
+    }
+    let transferErrors = await client.createTransfers([
+      {
+        id: 1n,
+        debit_account_id: 1n,
+        credit_account_id: 2n,
+        amount: 10n,
+        pending_id: 0n,
+        user_data_128: 0n,
+        user_data_64: 0n,
+        user_data_32: 0,
+        timeout: 0,
+        ledger: 1,
+        code: 1,
+        flags: 0,
+        timestamp: 0n,
+      },
+    ]);
+    for (const error of transferErrors) {
+      console.error(
+        `Batch transfer at ${error.index} failed to create: ${CreateTransferError[error.result]}.`,
+      );
+    }
+    let accounts = await client.lookupAccounts([1n, 2n]);
+    for (let account of accounts) {
+      if (account.id === 1n) {
+        console.log("All Good");
+      } else if (account.id === 2n) {
+        console.log("All Good");
+      } else {
+        console.error(`fail`);
+      }
+    }
+    return { event, body: "All good!" };
   },
 );
 
