32291216f867f32039a73877307488016f73d068
Johnny Wu <jonathan.x.wu@gmail.com>
2024-02-28 23:34:38 -0500

    Clenanup linting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -48,8 +48,6 @@ const Dashboard = async () => {
           src="https://giphy.com/embed/3ohs7HdhQA4ffttvrO"
           width="480"
           height="480"
-          frameBorder="0"
-          class="giphy-embed"
           allowFullScreen
         ></iframe>
         <p></p>
