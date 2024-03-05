cb3d21f907c26649fcfae3c75a114da037a09fd9
Johnny Wu <jonathan.x.wu@gmail.com>
2024-03-05 11:57:53 -0500

    Add price action dropdown

--- src/components/library/NavbarComponent.tsx
+++ src/components/library/NavbarComponent.tsx
@@ -26,46 +26,49 @@ export default function NavbarComponent() {
     },
   ];
 
+  const PriceAction = ["Bitcoin", "USD", "GOLD", "USDC", "USDT"];
+
   const DataDropwdown = (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-          Data Insights
+          Price Action
           <ChevronDownIcon
             className="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
             aria-hidden="true"
           />
         </Menu.Button>
       </div>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-          <div className="px-1 py-1 ">
+      {PriceAction.map((item) => (
+        <Transition
+          key={item}
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
             <Menu.Item>
-              {({ active }) => (
+              <div className="px-1 py-1">
                 <button
-                  className={`${active ? "bg-violet-500 text-white" : "text-gray-900"} group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  className={`text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                 >
-                  Bitcion
+                  {item}
                 </button>
-              )}
+              </div>
             </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Transition>
+          </Menu.Items>
+        </Transition>
+      ))}
     </Menu>
   );
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
+      {({ open = true }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
