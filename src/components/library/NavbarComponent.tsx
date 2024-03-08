260f9baca99fa6c90469d7b34a07032d9f42b034
Johnny Wu <jonathan.x.wu@gmail.com>
2024-03-08 12:23:58 +0000

    Refactor navbar menu components

--- src/components/library/NavbarComponent.tsx
+++ src/components/library/NavbarComponent.tsx
@@ -64,6 +64,29 @@ export default function NavbarComponent() {
     </Menu>
   );
 
+  // @ts-ignore
+  const MobileMenu = ({ open }) => (
+    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+      <span className="absolute -inset-0.5" />
+      <span className="sr-only">Open main menu</span>
+      {open ? (
+        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+      ) : (
+        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+      )}
+    </Disclosure.Button>
+  );
+
+  const Logo = () => (
+    <div className="flex flex-shrink-0 items-center">
+      <img
+        className="h-8 w-auto"
+        src="https://avatars.githubusercontent.com/u/139788058?s=200&v=4"
+        alt="Your Company"
+      />
+    </div>
+  );
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open = true }) => (
@@ -71,25 +94,10 @@ export default function NavbarComponent() {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+                <MobileMenu open={open} />
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://avatars.githubusercontent.com/u/139788058?s=200&v=4"
-                    alt="Your Company"
-                  />
-                </div>
+                <Logo />
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
@@ -194,7 +202,6 @@ export default function NavbarComponent() {
               </div>
             </div>
           </div>
-
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
