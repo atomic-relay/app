2512d08876fac109ce43c5d0d70ea9368e17c82f
Johnny Wu <jonathan.x.wu@gmail.com>
2024-03-08 10:14:36 +0000

    Cleanup nav bar

--- src/components/library/NavbarComponent.tsx
+++ src/components/library/NavbarComponent.tsx
@@ -26,12 +26,11 @@ export default function NavbarComponent() {
     },
   ];
 
-  const PriceAction = ["Bitcoin", "USD", "GOLD", "USDC", "USDT"];
+  const PriceAction = ["BTC", "USD", "USDT"];
 
   const DataDropwdown = (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <a href="/api/auth/login">Login</a>
         <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
           Price Action
           <ChevronDownIcon
