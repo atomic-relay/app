7641e04d1842819b1f788843eaff6566f355b979
Johnny Wu <jonathan.x.wu@gmail.com>
2024-03-10 19:38:49 +0000

    Dropdown refactor

--- src/components/library/NavbarComponent.tsx
+++ src/components/library/NavbarComponent.tsx
@@ -117,6 +117,67 @@ export default function NavbarComponent() {
     </div>
   );
 
+  const Notification = () => (
+    <button
+      type="button"
+      className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+    >
+      <span className="absolute -inset-1.5" />
+      <span className="sr-only">View notifications</span>
+      <BellIcon className="h-6 w-6" aria-hidden="true" />
+    </button>
+  );
+
+  const LoggedInState = () => (
+    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+      <SignedIn>
+        <div className="flex flex-row gap-5">
+          <Avatar
+            title={"Johnny Wu"}
+            src={"https://avatars.githubusercontent.com/u/5092624?v=4"}
+            topStatus="online"
+            size="sm"
+          />
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </Menu.Button>
+  );
+
+  const SettingsDropdown = () => (
+    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <Menu.Item>
+        {({ active }) => (
+          <a
+            href="#"
+            className={classNames(
+              active ? "bg-gray-100" : "",
+              "block px-4 py-2 text-sm text-gray-700",
+            )}
+          >
+            Your Profile
+          </a>
+        )}
+      </Menu.Item>
+      <Menu.Item>
+        {({ active }) => (
+          <a
+            href="#"
+            className={classNames(
+              active ? "bg-gray-100" : "",
+              "block px-4 py-2 text-sm text-gray-700",
+            )}
+          >
+            Settings
+          </a>
+        )}
+      </Menu.Item>
+    </Menu.Items>
+  );
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open = true }) => (
@@ -131,33 +192,9 @@ export default function NavbarComponent() {
                 <NavItems />
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                <Notification />
                 <Menu as="div" className="relative ml-3">
-                  <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <SignedIn>
-                      <div className="flex flex-row gap-5">
-                        <Avatar
-                          title={"Johnny Wu"}
-                          src={
-                            "https://avatars.githubusercontent.com/u/5092624?v=4"
-                          }
-                          topStatus="online"
-                          size="sm"
-                        />
-                        <UserButton afterSignOutUrl="/" />
-                      </div>
-                    </SignedIn>
-                    <SignedOut>
-                      <SignInButton />
-                    </SignedOut>
-                  </Menu.Button>
+                  <LoggedInState />
                   <Transition
                     as={Fragment}
                     enter="transition ease-out duration-100"
@@ -167,47 +204,7 @@ export default function NavbarComponent() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700",
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
+                    <SettingsDropdown />
                   </Transition>
                 </Menu>
               </div>
