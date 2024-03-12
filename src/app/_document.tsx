ccec502c74ea3c302e0a013ce8cc5e2a10ca3a76
Johnny Wu <jonathan.x.wu@gmail.com>
2024-03-12 16:50:43 -0400

    Remove use of metricalp

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -1,27 +1,13 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        {/*<script*/}
-        {/*  src="https://cdn.metricalp.com/event/metricalp.js"*/}
-        {/*  data-tid="mam6"*/}
-        {/*  defer*/}
-        {/*></script>*/}
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
-        {/*<Script*/}
-        {/*  src="https://cdn.metricalp.com/event/metricalp.js"*/}
-        {/*  data-tid="mam6"*/}
-        {/*  defer*/}
-        {/*  onError={() => console.error("error")}*/}
-        {/*  onReady={() => console.log("true")}*/}
-        {/*/>*/}
       </body>
     </Html>
   );
